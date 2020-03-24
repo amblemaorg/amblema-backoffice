e5df63f962381e7dfe6cdd0d54e140340dd84005
franklinp <franklin@binauraldev.com>
2020-03-23 21:36:03 -0400

    IMP: Tabla agregada #151, estilos aplicados #152

--- src/app/pages/request/creation-requests/creation-requests.component.ts
+++ src/app/pages/request/creation-requests/creation-requests.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseTable } from 'src/app/helpers/base-table';
+import { ACTION } from 'src/app/helpers/text-content/text-crud';
 
 @Component({
   selector: 'app-creation-requests',
   templateUrl: './creation-requests.component.html',
   styleUrls: ['./creation-requests.component.scss']
 })
-export class CreationRequestsComponent implements OnInit {
+export class CreationRequestsComponent extends BaseTable implements OnInit {
 
-  constructor() { }
+  data: any = [
+    {
+      idRequest: '000',
+      idProject: '101101',
+      applicant: 'Jose Jose', 
+      date: '09/80/2000',
+      status: 'Activo',
+      type: 'Coordinador'
+    }
+  ];
+
+  constructor() { super(); }
 
   ngOnInit() {
+    this.settings.actions = {
+      columnTitle: 'Acciones',
+      add: false,
+      edit: false,
+      //  Fake action
+      delete: true,
+      custom: [
+        { name: ACTION.VIEW, title: '<i class="far fa-eye fa-sm"></i>' },
+        { name: ACTION.DELETE, title: '<i class="nb-trash"></i>' }
+      ]
+    },
+
+      this.settings.columns = {
+        idRequest: {
+          title: 'N° de la solicitud',
+          type: 'string',
+        },
+        idProject: {
+          title: 'ID del proyecto',
+          type: 'string'
+        },
+        type: {
+          title: 'Tipo de solicitante',
+          type: 'string'
+        },
+        applicant: {
+          title: 'Solicitante',
+          type: 'string'
+        },
+        date: {
+          title: 'Fecha',
+          type: 'string'
+        },
+        status: {
+          title: 'Estatus',
+          type: 'string '
+        }       
+      };    
   }
 
 }

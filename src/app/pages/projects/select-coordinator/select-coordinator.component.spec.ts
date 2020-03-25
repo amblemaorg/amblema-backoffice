ad271dcebed8e1d66317ccec38cf38a286a8ee41
franklinp <franklin@binauraldev.com>
2020-03-25 14:12:03 -0400

    WIP: Mock data para encontrar los coordinadores #187

--- src/app/pages/projects/select-coordinator/select-coordinator.component.spec.ts
+++ src/app/pages/projects/select-coordinator/select-coordinator.component.spec.ts
@@ -3,11 +3,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SelectCoordinatorComponent } from './select-coordinator.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { Store, NgxsModule } from '@ngxs/store';
-import { CoordinatorUserState } from 'src/app/store/user-store/coordinator-user.action';
+import { CoordinatorUserState, GetCoordinatorUsers, SetCoordinatorUser } from 'src/app/store/user-store/coordinator-user.action';
 import { Utility } from 'src/app/helpers/utility';
 import { NbToastrModule, NbThemeModule } from '@nebular/theme';
 import { CustomToastrService } from 'src/app/services/helper/custom-toastr.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CoordinatorUser } from 'src/app/models/user/coordinator-user.model';
 
 describe('SelectCoordinatorComponent', () => {
   let component: SelectCoordinatorComponent;
@@ -29,7 +30,7 @@ describe('SelectCoordinatorComponent', () => {
         Utility ]
     })
     .compileComponents();
-    store = TestBed.get(store);
+    store = TestBed.get(Store);
   }));
 
   beforeEach(() => {
@@ -43,10 +44,36 @@ describe('SelectCoordinatorComponent', () => {
   });
 
   it('TestCase#3129 - should find coordinator', () => {
+
+    const coordinator: CoordinatorUser = {
+      id: 'id',
+      name: 'usuario',
+      email: 'usuairo@gmail.com',
+      password: '2324234',
+      userType: '2', 
+      phone: '23423423',
+      role: '34234234',
+      addressState: 'Street',
+      addressMunicipality: 'Palavecino',
+      addressCity: 'Barquisimento',
+      address: 'Cabudare', 
+      gender: '2',
+      addressHome: 'Edificio',
+      firstName: 'usuario',
+      lastName: 'usuari', 
+      cardId: '234234324',
+      cardType:'V',
+      birthdate: '12/12/12',
+      homePhone: '234234324',
+      status: 'Activo'
+    };
+
+    store.dispatch( new SetCoordinatorUser(coordinator));
+    store.dispatch( new GetCoordinatorUsers() ); 
+
+    const user = store.selectSnapshot(state => state);  
+    expect( user ).toBeDefined();
     
-
-
-
   });
 
   it('TestCase#3129 - should select coordinador after submit', () => {

e401fc0570aa86e26981c9c83827e78988bc4b20
franklinp <franklin@binauraldev.com>
2020-04-10 21:57:13 -0400

    WIP: Asignado el lapso en el toggle #189

--- src/app/pages/main-content/peca-setting/admin-year-lapses/admin-lapses/activity-board/activity-board.component.ts
+++ src/app/pages/main-content/peca-setting/admin-year-lapses/admin-lapses/activity-board/activity-board.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { BaseTable, TableActions } from 'src/app/helpers/base-table';
 import { SpecialToggleComponent } from '../special-toggle/special-toggle.component';
 import { ModalService } from 'src/app/services/helper/modal.service';
-import { Select } from '@ngxs/store';
-import { LapseActivityState } from 'src/app/store/lapse-activities.action';
-import { Observable } from 'rxjs';
+import { Select, Store } from '@ngxs/store';
+import { LapseActivityState, GetLapActivities } from 'src/app/store/lapse-activities.action';
+import { Observable, Subscription } from 'rxjs';
 import { LapseActivity } from 'src/app/models/lapse-activities.model';
 
 @Component({
@@ -15,40 +15,55 @@ import { LapseActivity } from 'src/app/models/lapse-activities.model';
 export class ActivityBoardComponent extends BaseTable implements TableActions, OnInit {
 
   @Select(LapseActivityState.lapses) lapses$: Observable<LapseActivity>;
+  subscription: Subscription; 
 
   @Input() lapse: string;
 
   data: Array<any>;
 
   constructor(
+    private store: Store, 
     public modalService: ModalService
   ) {
     super('form-activity-lapse');
-
   }
 
-  ngOnInit(): void {
+  ngOnInit()  {
 
     // Prepare the source data table
-    this.lapses$.subscribe(response => {
+    this.subscription =  this.lapses$.subscribe(response => {
 
       /* Give each activity a lapse */
 
       if (this.lapse === '1') {
         this.data = response.lapse1;
-        this.data.forEach(value => {
-          value.lapse = '1';
-        });
+
+        this.data = Object.assign([], this.data);
+
+        for (let index = 0; index < this.data.length; index++) {
+          this.data[index] = Object.assign({}, this.data[index], { lapse: '1' });
+        }
+        this.source.load( this.data ); 
       } else if (this.lapse === '2') {
+        
         this.data = response.lapse2;
-        this.data.forEach(value => {
-          value.lapse = '2';
-        });
+
+        this.data = Object.assign([], this.data);
+
+        for (let index = 0; index < this.data.length; index++) {
+          this.data[index] = Object.assign({}, this.data[index], { lapse: '2' });
+        }
+        this.source.load( this.data ); 
       } else {
+
         this.data = response.lapse3;
-        this.data.forEach(value => {
-          value.lapse = '3';
-        });
+
+        this.data = Object.assign([], this.data);
+
+        for (let index = 0; index < this.data.length; index++) {
+          this.data[index] = Object.assign({}, this.data[index], { lapse: '3' });
+        }
+        this.source.load( this.data ); 
       }
     });
 
@@ -63,7 +78,7 @@ export class ActivityBoardComponent extends BaseTable implements TableActions, O
         title: 'Estatus',
         type: 'custom',
         renderComponent: SpecialToggleComponent,
-        sort: true,
+        sort: true, 
         filter: false,
         onComponentInitFunction(instance: any) {
           instance.save.subscribe();
@@ -73,4 +88,9 @@ export class ActivityBoardComponent extends BaseTable implements TableActions, O
   }
 
   onAction(event: any): void { }
+
+  onUpdateState( value: any ) {
+      this.store.dispatch( new GetLapActivities() ); 
+  }
+
 }

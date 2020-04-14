7874797d51af94b44cd0437b653f3e3d7fb2c5cc
franklinp <franklin@binauraldev.com>
2020-04-14 15:51:02 -0400

    WIP: Linting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
-  // api: 'http://157.245.131.248:10506/', // <--- QA
+  production: true,
+   api: 'http://157.245.131.248:10506/', // <--- QA
   // api: 'http://157.245.131.248:10507/', // <--- Staging
-  api: 'http://0.0.0.0:10505/'
+ // api: 'http://0.0.0.0:10505/'
  // api: 'http://190.207.178.184:10505/'
 };

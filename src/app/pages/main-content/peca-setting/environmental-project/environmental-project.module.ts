1f6af39dbd6dff03f914c4d8ad5fcd2aa480955f
franklinp <franklin@binauraldev.com>
2020-04-15 14:59:20 -0400

    WIP: Lista generica creada, desarrollando el formulario de objectivos #194

--- src/app/pages/main-content/peca-setting/environmental-project/environmental-project.module.ts
+++ src/app/pages/main-content/peca-setting/environmental-project/environmental-project.module.ts
@@ -3,24 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { EnvironmentalProjectRoutingModule } from './environmental-project-routing.module';
 import { MainFormComponent } from './main-form/main-form.component';
-import { LapseFormComponent } from './lapse-form/lapse-form.component';
 import { ObjectiveFormComponent } from './objective-form/objective-form.component';
 import { TopicsFormComponent } from './topics-form/topics-form.component';
 import { EnvironmentalProjectComponent } from './environmental-project.component';
-import { NbCardModule, NbRadioModule, NbAlertModule, NbIconModule } from '@nebular/theme';
+import { NbCardModule, NbRadioModule, NbAlertModule, NbIconModule, NbButtonModule, NbListModule } from '@nebular/theme';
 import { ReactiveInputModule } from 'src/app/pages/components/form-components/reactive-input/reactive-input.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ListItemsComponent } from './list-items/list-items.component';
 
 
 @NgModule({
-  declarations: [MainFormComponent, LapseFormComponent, ObjectiveFormComponent, TopicsFormComponent, EnvironmentalProjectComponent],
+  declarations: [MainFormComponent, ObjectiveFormComponent, TopicsFormComponent, EnvironmentalProjectComponent, ListItemsComponent],
   imports: [
+    ReactiveFormsModule,
     CommonModule,
     NbCardModule,
     ReactiveInputModule,
     NbRadioModule,
     FormsModule,
     NbAlertModule,
+    NbButtonModule,
+    NbListModule,
     NbIconModule,
     EnvironmentalProjectRoutingModule
   ]

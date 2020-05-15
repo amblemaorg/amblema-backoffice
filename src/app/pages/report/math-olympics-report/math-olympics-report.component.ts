7058c6a46d63f355f1858dc7d9b646f8a6bb51c4
franklinp <franklin@binauraldev.com>
2020-05-15 11:23:46 -0400

    WIP : Filtro implementado. Desarrollando el servicio para consultar las olimpidas. #219

--- src/app/pages/report/math-olympics-report/math-olympics-report.component.ts
+++ src/app/pages/report/math-olympics-report/math-olympics-report.component.ts
@@ -1,48 +1,59 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { PDFReportMath } from './pdf-report-math.service';
-import { DatePipe } from '@angular/common';
-import { MathOlympicsReportService } from 'src/app/services/report/math-olympics-report.service';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { PDFReportMath } from "./pdf-report-math.service";
+import { DatePipe } from "@angular/common";
+import { MathOlympicsReportService } from "src/app/services/report/math-olympics-report.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-math-olympics-report',
-  templateUrl: './math-olympics-report.component.html',
-  styleUrls: ['./math-olympics-report.component.scss'],
-  providers: [ PDFReportMath, DatePipe ]
+  selector: "app-math-olympics-report",
+  templateUrl: "./math-olympics-report.component.html",
+  styleUrls: ["./math-olympics-report.component.scss"],
+  providers: [PDFReportMath, DatePipe],
 })
 export class MathOlympicsReportComponent implements OnInit, OnDestroy {
-
   dateInitSelected: any = null;
-  dateEndSelected: any = null; 
+  dateEndSelected: any = null;
 
-  datesInit = new Array<any>();  
+  datesInit = new Array<any>();
   datesEnd = new Array<any>();
 
   subscriptionService: Subscription;
 
   constructor(
     private mathOlympicsReportService: MathOlympicsReportService,
-    private generateReporte: PDFReportMath ) {
-  
-    }
+    private generateReporte: PDFReportMath
+  ) {}
 
   async ngOnInit() {
-    
-    this.subscriptionService = this.mathOlympicsReportService.getSchoolYears().subscribe( response => {
-      
-      this.datesInit = response;
-      this.datesEnd = response;
-
-    });  
+    this.subscriptionService = this.mathOlympicsReportService
+      .getSchoolYears()
+      .subscribe((response) => {
+        this.datesInit = response;
+        this.datesEnd = response;
+      });
   }
 
   async ngOnDestroy() {
-    if ( this.subscriptionService ) { this.subscriptionService.unsubscribe(); }
+    if (this.subscriptionService) {
+      this.subscriptionService.unsubscribe();
+    }
   }
 
   onGenerateReport() {
-
     this.generateReporte.generateMathOlympics();
+  }
 
+  onChangeDateInit(event: any) {
+    let position: any;
+
+    this.datesInit.forEach((value, index) => {
+      if (value.id === event) {
+        position = index;
+      }
+    });
+
+    this.datesEnd = this.datesInit.filter( (value, index) => position < index );  
+
+    if( !this.datesEnd.length ) this.dateEndSelected = null;
   }
 }

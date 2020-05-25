ba2c344b16cb064f281c04b2bb07ab639ba687bc
franklinp <franklin@binauraldev.com>
2020-05-25 09:26:58 -0400

    WIP: Creando vista #234

---
+++ src/app/pages/report/graphics-report/sponsor-status/sponsor-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sponsor-status',
+  templateUrl: './sponsor-status.component.html',
+  styleUrls: ['./sponsor-status.component.scss']
+})
+export class SponsorStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/report/report-routing.module.ts
+++ src/app/pages/report/report-routing.module.ts
@@ -1,51 +1,72 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: 'diagnostic-report',
-    loadChildren: () => import('./diagnostic-report/diagnostic-report.module').
-    then(m => m.DiagnosticReportModule)
+    path: "diagnostic-report",
+    loadChildren: () =>
+      import("./diagnostic-report/diagnostic-report.module").then(
+        (m) => m.DiagnosticReportModule
+      ),
   },
   {
-    path: 'sponsor-report',
-    loadChildren: () => import('./sponsor-report/sponsor-report.module')
-    .then( m => m.SponsorReportModule )
+    path: "sponsor-report",
+    loadChildren: () =>
+      import("./sponsor-report/sponsor-report.module").then(
+        (m) => m.SponsorReportModule
+      ),
   },
   {
-    path: 'coordinator-report',
-    loadChildren: () => import('./coordinator-report/coordinator-report.module')
-    .then( m => m.CoordinatorReportModule )
+    path: "coordinator-report",
+    loadChildren: () =>
+      import("./coordinator-report/coordinator-report.module").then(
+        (m) => m.CoordinatorReportModule
+      ),
   },
   {
-    path: 'school-report',
-    loadChildren: () => import('./school-report/school-report.module')
-    .then( m => m.SchoolReportModule )
+    path: "school-report",
+    loadChildren: () =>
+      import("./school-report/school-report.module").then(
+        (m) => m.SchoolReportModule
+      ),
   },
   {
-    path: 'teacher-report',
-    loadChildren: () => import('./teacher-report/teacher-report.module')
-    .then( m => m.TeacherReportModule )
+    path: "teacher-report",
+    loadChildren: () =>
+      import("./teacher-report/teacher-report.module").then(
+        (m) => m.TeacherReportModule
+      ),
   },
   {
-    path: 'math-olympics-report',
-    loadChildren: () => import('./math-olympics-report/math-olympics-report.module').
-    then(m => m.MathOlympicsReportModule)
+    path: "math-olympics-report",
+    loadChildren: () =>
+      import("./math-olympics-report/math-olympics-report.module").then(
+        (m) => m.MathOlympicsReportModule
+      ),
   },
 
   /**
    * Graphics report module
    */
 
-   {
-     path: 'enrolled-schools-report',
-     loadChildren: () => import('./graphics-report/enrolled-schools-report/enrolled-schools-report.module').
-     then(m => m.EnrolledSchoolsReportModule)
-   }
+  {
+    path: "enrolled-schools-report",
+    loadChildren: () =>
+      import(
+        "./graphics-report/enrolled-schools-report/enrolled-schools-report.module"
+      ).then((m) => m.EnrolledSchoolsReportModule),
+  },
+  {
+    path: "sponsor-status",
+    loadChildren: () =>
+      import("./graphics-report/sponsor-status/sponsor-status.module").then(
+        (m) => m.SponsorStatusModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ReportRoutingModule { }
+export class ReportRoutingModule {}

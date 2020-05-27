3e202f96ab8d8f7c95c7dfc60543c5bb0312d37e
franklinp <franklin@binauraldev.com>
2020-05-27 15:30:23 -0400

    IMP: Vista creada #243 estilos #244 adaptacion #245

--- src/app/pages/request/amblema-confirmation-request/amblema-confirmation-request.component.ts
+++ src/app/pages/request/amblema-confirmation-request/amblema-confirmation-request.component.ts
@@ -1,22 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { BaseTable } from 'src/app/helpers/base-table';
-import { Store } from '@ngxs/store';
+import { Component, OnInit } from "@angular/core";
+import { BaseTable } from "src/app/helpers/base-table";
+import { Store } from "@ngxs/store";
+import { EventManager } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-amblema-confirmation-request',
-  templateUrl: './amblema-confirmation-request.component.html',
-  styleUrls: ['./amblema-confirmation-request.component.scss']
+  selector: "app-amblema-confirmation-request",
+  templateUrl: "./amblema-confirmation-request.component.html",
+  styleUrls: ["./amblema-confirmation-request.component.scss"],
 })
-export class AmblemaConfirmationRequestComponent extends BaseTable implements OnInit {
-
-  constructor(
-    private store: Store,
-  
-  ) {
+export class AmblemaConfirmationRequestComponent extends BaseTable
+  implements OnInit {
+  constructor(private store: Store) {
     super();
 
     this.settings.actions = {
-      columnTitle: 'Acciones',
+      columnTitle: "Acciones",
       add: false,
       edit: false,
       //  Fake action
@@ -31,34 +29,40 @@ export class AmblemaConfirmationRequestComponent extends BaseTable implements On
 
     this.settings.columns = {
       code: {
-        title: 'N° de la solicitud',
-        type: 'string'
+        title: "N° de la solicitud",
+        type: "string",
       },
       id: {
-        title: 'Id del proyecto',
-        type: 'string'
+        title: "Id del proyecto",
+        type: "string",
       },
       coordinator: {
-        title: '',
-        type: 'string'
+        title: "Coordinador",
+        type: "string",
       },
       sponsor: {
-        title: 'Id del proyecto',
-        type: 'string'
+        title: "Padrino",
+        type: "string",
       },
       school: {
-        title: 'Id del proyecto',
-        type: 'string'
+        title: "Escuela",
+        type: "string",
       },
       status: {
-        title: 'Estatus',
-        type: 'string'
-      }
+        title: "Estatus",
+        type: "string",
+      },
+    };
+  }
+
+  ngOnInit() {}
+
+  onAction(event) {
+    switch (event.action) {
+      case this.ACTION.EDIT:
+        break;
+      case this.ACTION.DELETE:
+        break;
     }
-
   }
-
-  ngOnInit() {
-  }
-
 }

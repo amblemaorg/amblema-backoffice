c649b94e02525a60eba9db2163823ffe6625b3a7
franklinp <franklin@binauraldev.com>
2020-06-19 09:09:51 -0400

    IMP: Cambio de contrasena implementado #263

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,18 +3,26 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RouterModule } from '@angular/router';
-import { NbAuthModule, NbPasswordAuthStrategy, NbAuthJWTToken, NbTokenStorage, NbTokenLocalStorage } from '@nebular/auth';
-import { NbAlertModule, NbCheckboxModule, NbButtonModule, NbInputModule } from '@nebular/theme';
+import {
+  NbAuthModule,
+  NbPasswordAuthStrategy,
+  NbAuthJWTToken,
+  NbTokenStorage,
+  NbTokenLocalStorage,
+} from '@nebular/auth';
+import {
+  NbAlertModule,
+  NbCheckboxModule,
+  NbButtonModule,
+  NbInputModule,
+} from '@nebular/theme';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { environment } from 'src/environments/environment.prod';
 import { RequestPasswordComponent } from './request-password/request-password.component';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RequestPasswordComponent,
-  ],
+  declarations: [LoginComponent, RequestPasswordComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
@@ -37,14 +45,12 @@ import { RequestPasswordComponent } from './request-password/request-password.co
      */
     NbAuthModule.forRoot({
       strategies: [
-
         NbPasswordAuthStrategy.setup({
-
           name: 'email',
           baseEndpoint: `${environment.api}`,
           token: {
             class: NbAuthJWTToken,
-            key: 'access_token'
+            key: 'access_token',
           },
           refreshToken: true,
           login: {
@@ -52,19 +58,42 @@ import { RequestPasswordComponent } from './request-password/request-password.co
             method: 'post',
             redirect: {
               success: '/pages/dashboard',
-              failure: null
+              failure: null,
             },
-            defaultErrors: ['La combinación de inicio de sesión / correo electrónico no es correcta, intente nuevamente.'],
+            defaultErrors: [
+              'La combinación de inicio de sesión / correo electrónico no es correcta, intente nuevamente.',
+            ],
             defaultMessages: ['Has ingresado exitosamente.'],
           },
-        })
+          requestPass: {
+            endpoint: 'auth/passwordrecovery',
+            method: 'post',
+            redirect: {
+              success: '/auth/login',
+              failure: null,
+            },
+            defaultErrors: ['Algo salió mal. Por favor, vuelva a intentarlo.'],
+            defaultMessages: [
+              'Las instrucciones para restablecer la contraseña se han enviado a su correo electrónico.',
+            ],
+          },
+        }),
       ],
       forms: {
+        requestPassword: {
+          redirectDelay: 2500,
+          strategy: 'email',
+          showMessages: {
+            success: true,
+            error: true,
+          },
+        },
         login: {
           redirectDelay: 500, // delay before redirect after a successful login, while success message is shown to the user
-          strategy: 'email',  // strategy id key.
-          rememberMe: true,   // whether to show or not the `rememberMe` checkbox
-          showMessages: {     // show/not show success/error messages
+          strategy: 'email', // strategy id key.
+          rememberMe: true, // whether to show or not the `rememberMe` checkbox
+          showMessages: {
+            // show/not show success/error messages
             success: true,
             error: true,
           },
@@ -79,14 +108,14 @@ import { RequestPasswordComponent } from './request-password/request-password.co
             required: true,
           },
         },
-      }
+      },
     }),
   ],
   providers: [
     {
       provide: NbTokenStorage,
-      useClass: NbTokenLocalStorage
-    }
-  ]
+      useClass: NbTokenLocalStorage,
+    },
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}

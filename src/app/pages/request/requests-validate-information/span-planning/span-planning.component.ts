7a0231f937a61b2e62f3dc62fa87039e2eb1c364
franklinp <franklin@binauraldev.com>
2020-06-26 14:45:21 -0400

    IMP: Modal creado, planificacion de lapso #261 Estilos aplicados #260

---
+++ src/app/pages/request/requests-validate-information/span-planning/span-planning.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { InformationDetailsComponent } from '../information-details/information-details.component';
+
+@Component({
+  selector: 'app-span-planning',
+  templateUrl: './span-planning.component.html',
+  styleUrls: ['./span-planning.component.scss']
+})
+export class SpanPlanningComponent extends InformationDetailsComponent {
+
+}

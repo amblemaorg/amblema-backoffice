39cd6e67ac0df00df8a987b15df5f8511b3526b3
franklinp <franklin@binauraldev.com>
2020-06-29 14:52:43 -0400

    IMP: Modal detalles de la escuela implementado, para la solicitud de proyecto #265 Estilos aplicados #266 Responsive aplicado #267

--- src/app/pages/request/requests-validate-information/slider-details/slider-details.component.ts
+++ src/app/pages/request/requests-validate-information/slider-details/slider-details.component.ts
@@ -1,11 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { InformationDetailsComponent } from '../information-details/information-details.component';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-slider-details',
   templateUrl: './slider-details.component.html',
   styleUrls: ['./slider-details.component.scss']
 })
-export class SliderDetailsComponent extends InformationDetailsComponent {
+export class SliderDetailsComponent extends InformationDetailsComponent implements OnInit {
 
+  customOptions: OwlOptions = {
+    stagePadding: 50,
+
+    autoHeight: true,
+    margin: 70,
+    dots: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  };
+
+  show = false;
+
+  ngOnInit() {
+    setTimeout(() => this.show = true);
+  }
 }

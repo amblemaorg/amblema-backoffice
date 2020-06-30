6e7fac5ecb43425cfa8a831d8c1df0bfa2d8edd2
franklinp <franklin@binauraldev.com>
2020-06-29 22:16:39 -0400

    MOD: Refactorizacion del selector del rol

--- src/app/pages/user/_shared/reactive-select/reactive-select.module.ts
+++ src/app/pages/user/_shared/reactive-select/reactive-select.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectRoleComponent } from './select-role/select-role.component';
-
-
+import { NgSelectModule } from '@ng-select/ng-select';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SelectRoleComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, NgSelectModule, ReactiveFormsModule, FormsModule ],
+  exports: [SelectRoleComponent],
 })
-export class ReactiveSelectModule { }
+export class ReactiveSelectModule {}

96c3c3a85de5f23c32106773f25ce20675939d36
franklinp <franklin@binauraldev.com>
2020-06-29 23:48:11 -0400

    MOD: Modificacion del campo email

--- src/app/pages/user/_shared/user-form-abstract.ts
+++ src/app/pages/user/_shared/user-form-abstract.ts
@@ -1,16 +1,20 @@
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {
-  CYRILLIC_PATTERN,
-} from './reactive-input/_shared/validation-patterns';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { CYRILLIC_PATTERN } from "./reactive-input/_shared/validation-patterns";
+import { EMAIL_PATTERN } from "../../_components/form-components/shared/constant/validation-patterns-list";
 
 export abstract class UserForm {
   public nameMessage = `Nombre inválido. Debes agregar solo letras en este campo`;
+  public lastNameMessage = `Apellido inválido. Debes agregar solo letras en este campo`;
+  public emailMessage = `El correo electrónico no corresponde a un formato correcto`;
 
   public form: FormGroup = new FormGroup({
     name: new FormControl(),
-    email: new FormControl(),
+    email: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(EMAIL_PATTERN),
+    ]),
     password: new FormControl(),
-    role: new FormControl(),
+    role: new FormControl(null, [Validators.required]),
     addressState: new FormControl(),
     addressMunicipality: new FormControl(),
     addressCity: new FormControl(),
@@ -19,7 +23,6 @@ export abstract class UserForm {
 }
 
 export abstract class UserAdminForm extends UserForm {
-  public lastNameMessage = `Apellido inválido. Debes agregar solo letras en este campo`;
   public functionMessage = `Dato inválido, agrega sólo letras`;
 
   constructor() {
@@ -35,7 +38,7 @@ export abstract class UserAdminForm extends UserForm {
         Validators.required,
         Validators.pattern(CYRILLIC_PATTERN),
       ]),
-      cardType: new FormControl('1', [Validators.required]),
+      cardType: new FormControl("1", [Validators.required]),
       cardId: new FormControl(null),
       phone: new FormControl(),
       function: new FormControl(null, [

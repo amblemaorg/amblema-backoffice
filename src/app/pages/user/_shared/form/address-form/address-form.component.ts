85384e9504930b06d4195c3468e416d77f0b3764
franklinp <franklin@binauraldev.com>
2020-07-04 23:38:34 -0400

    MOD: Crud implementado en los municipios.

--- src/app/pages/user/_shared/form/address-form/address-form.component.ts
+++ src/app/pages/user/_shared/form/address-form/address-form.component.ts
@@ -1,21 +1,25 @@
-import { Component, Input } from '@angular/core';
-import { AbstractControl, FormControl, Validators } from '@angular/forms';
-import { Select, Store } from '@ngxs/store';
+import { Component, Input, OnDestroy } from "@angular/core";
+import { AbstractControl, FormControl, Validators } from "@angular/forms";
+import { Select, Store } from "@ngxs/store";
 import {
   AddressState,
   SetStateSelected,
-} from 'src/app/store/_address/address.action';
-import { Observable } from 'rxjs';
-import { Statal, Municipality } from 'src/app/_models/address.model';
-import { FORM_MODALITY } from '../../abstract-form-mode';
-import { AddressService } from 'src/app/services/address.service';
+  SetMunicipality,
+  DeleteMunicipality,
+  UpdateMunicipality,
+} from "src/app/store/_address/address.action";
+import { Observable, Subscription } from "rxjs";
+import { Statal, Municipality } from "src/app/_models/address.model";
+import { FORM_MODALITY } from "../../abstract-form-mode";
+import { AddressService } from "src/app/services/address.service";
+import { CustomToastrService } from "src/app/services/helper/custom-toastr.service";
 
 @Component({
-  selector: 'app-address-form',
-  templateUrl: './address-form.component.html',
-  styleUrls: ['./address-form.component.scss'],
+  selector: "app-address-form",
+  templateUrl: "./address-form.component.html",
+  styleUrls: ["./address-form.component.scss"],
 })
-export class AddressFormComponent {
+export class AddressFormComponent implements OnDestroy {
   @Input() address: AbstractControl | null = new FormControl();
   @Input() addressState: AbstractControl | null = new FormControl();
   @Input() addressMunicipality: AbstractControl | null = new FormControl();
@@ -25,6 +29,8 @@ export class AddressFormComponent {
   @Select(AddressState.municipalities) municipalities$: Observable<
     Municipality[]
   >;
+
+  public subscriptionServices: Subscription;
 
   // -- Ng model input controller --
   addressStateSelected: string;
@@ -42,7 +48,17 @@ export class AddressFormComponent {
   // -- Define actions --
   readonly formMode = FORM_MODALITY;
 
-  constructor(public addressServices: AddressService, public store: Store) {}
+  constructor(
+    public toastService: CustomToastrService,
+    public addressServices: AddressService,
+    public store: Store
+  ) {  }
+
+  ngOnDestroy(): void {
+    if (this.subscriptionServices) {
+      this.subscriptionServices.unsubscribe();
+    }
+  }
 
   onAddressSelected(): void {
     this.addressState.setValue(this.addressStateSelected);
@@ -56,35 +72,81 @@ export class AddressFormComponent {
     }
   }
 
+  onSelectMunicipality(): void {
+    this.subscriptionServices = this.municipalities$.subscribe((response) => {
+      response.find((municipality) => {
+        if (municipality.id === this.addressMunicipalitySelected) {
+          this.control.setValue(municipality.name);
+        }
+      });
+    });
+  }
+
   onConfirmAction(mode: string): void {
     if (this.isEditionMode) {
       switch (mode) {
+        // -- Create --
         case FORM_MODALITY.CREATE.VALUE:
-          this.addressServices
+          this.subscriptionServices = this.addressServices
             .setMunicipality({
               name: this.control.value,
               state: this.addressStateSelected,
             })
+            .subscribe(
+              (response) => {
+                this.store.dispatch(new SetMunicipality(response));
+                this.toastService.registerSuccess(
+                  "Registro",
+                  "Municipio creado"
+                );
+                this.resetCrudMunicipality();
+              },
+              (err: any) => {
+                console.log(err);
+              }
+            );
+          break;
+        // -- Edit --
+        case FORM_MODALITY.EDIT.VALUE:
+          this.subscriptionServices = this.addressServices
+            .updateMunicipality(this.addressMunicipalitySelected, {
+              id: this.addressMunicipalitySelected,
+              state: this.addressStateSelected,
+              name: this.control.value,
+            })
             .subscribe((response) => {
+              this.toastService.updateSuccess(
+                "Actualización",
+                "Municipio actualizado"
+              );
+              this.store.dispatch(new UpdateMunicipality(response));
+              this.resetCrudMunicipality();
+              this.addressMunicipalitySelected = response.id;
+            });
+
+          break;
+        // -- Delete --
+        case FORM_MODALITY.DELETE.VALUE:
+          this.subscriptionServices = this.addressServices
+            .deleteMunicipality(this.addressMunicipalitySelected)
+            .subscribe((response) => {
+              this.store.dispatch(
+                new DeleteMunicipality(this.addressMunicipalitySelected)
+              );
+              this.toastService.deleteRegister(
+                "Eliminación",
+                "Se ha eliminado el municipio seleccionado"
+              );
               this.resetCrudMunicipality();
             });
-          break;
-        case FORM_MODALITY.EDIT.VALUE:
-          break;
-        case FORM_MODALITY.DELETE.VALUE:
           break;
       }
     }
   }
 
-  onCancelAction(): void {
-    if (this.isEditionMode) {
-      this.resetCrudMunicipality();
-    }
-  }
-
   resetCrudMunicipality(): void {
     this.isEditionMode = false;
+    this.addressMunicipalitySelected = null;
     this.isFormOn = null;
     this.control.reset();
   }

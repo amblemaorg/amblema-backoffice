e17a58c4735478df6df71c16fb38a2a177bb1dce
franklinp <franklin@binauraldev.com>
2020-07-06 15:05:23 -0400

    ProjectRequestsComponent

--- src/app/pages/_components/shared/map/geo-map/geo-map.component.ts
+++ src/app/pages/_components/shared/map/geo-map/geo-map.component.ts
@@ -7,18 +7,18 @@ import {
   Input,
   Output,
   EventEmitter,
-} from '@angular/core';
-import { MapsAPILoader } from '@agm/core';
+} from "@angular/core";
+import { MapsAPILoader } from "@agm/core";
 
 @Component({
-  selector: 'app-geo-map',
-  templateUrl: './geo-map.component.html',
-  styleUrls: ['./geo-map.component.scss'],
+  selector: "app-geo-map",
+  templateUrl: "./geo-map.component.html",
+  styleUrls: ["./geo-map.component.scss"],
 })
 export class GeoMapComponent implements OnInit {
   @Input() latitude: number;
   @Input() longitude: number;
-  @Input() label: string | null = 'Marca la ubicación exacta de la escuela';
+  @Input() label: string | null = "Marca la ubicación exacta de la escuela";
 
   @Output() laT = new EventEmitter<number>();
   @Output() longT = new EventEmitter<number>();
@@ -27,23 +27,22 @@ export class GeoMapComponent implements OnInit {
   address: string;
   private geoCoder;
 
-  @ViewChild('search', { static: false })
+  @ViewChild("search", { static: false })
   public searchElementRef: ElementRef;
 
   constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) {}
 
   ngOnInit() {
-    // load Places Autocomplete
+    // -- Load Places Autocomplete --
     this.mapsAPILoader.load().then(() => {
       this.setCurrentLocation();
       this.geoCoder = new google.maps.Geocoder();
     });
-
   }
 
-  // Get Current Location Coordinates
+  // -- Get Current Location Coordinates --
   private setCurrentLocation() {
-    if ('geolocation' in navigator) {
+    if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
         if (this.latitude === 0 && this.longitude === 0) {
           this.latitude = position.coords.latitude;
@@ -58,31 +57,31 @@ export class GeoMapComponent implements OnInit {
     this.latitude = $event.coords.lat;
     this.longitude = $event.coords.lng;
 
-    this.laT.emit( this.latitude );
-    this.longT.emit( this.longitude );
+    this.laT.emit(this.latitude);
+    this.longT.emit(this.longitude);
   }
 
   markerDragEnd($event: any) {
     this.latitude = $event.coords.lat;
     this.longitude = $event.coords.lng;
 
-    this.laT.emit( this.latitude );
-    this.longT.emit( this.longitude );
+    this.laT.emit(this.latitude);
+    this.longT.emit(this.longitude);
   }
 
   getAddress(latitude, longitude) {
     this.geoCoder.geocode(
       { location: { lat: latitude, lng: longitude } },
       (results, status) => {
-        if (status === 'OK') {
+        if (status === "OK") {
           if (results[0]) {
             this.zoom = 12;
             this.address = results[0].formatted_address;
           } else {
-            window.alert('No results found');
+            window.alert("No results found");
           }
         } else {
-          window.alert('Geocoder failed due to: ' + status);
+          window.alert("Geocoder failed due to: " + status);
         }
       }
     );

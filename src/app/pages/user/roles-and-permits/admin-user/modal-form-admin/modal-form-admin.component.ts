d2df3837efb8494a600fb13ba626c46e13319ef2
franklinp <franklin@binauraldev.com>
2020-07-14 08:48:52 -0400

    COR: Correccion de la sincronia para actualizar a los padrinos.

--- src/app/pages/user/roles-and-permits/admin-user/modal-form-admin/modal-form-admin.component.ts
+++ src/app/pages/user/roles-and-permits/admin-user/modal-form-admin/modal-form-admin.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { BsModalRef } from "ngx-bootstrap/modal";
-import { UserAdminForm } from "../../../_shared/abstract-user-form";
-import { FormModeService } from "../../../_services/form-mode.service";
-import { Subscription, Observable } from "rxjs";
-import { Store, Select } from "@ngxs/store";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { UserAdminForm } from '../../../_shared/abstract-user-form';
+import { FormModeService } from '../../../_services/form-mode.service';
+import { Subscription, Observable } from 'rxjs';
+import { Store, Select } from '@ngxs/store';
 import {
   BaseFormUser,
   FORM_MODALITY,
-} from "../.././../_shared/abstract-form-mode";
-import { ValidationService } from "../../../_shared/reactive-input/_shared/services/validation.service";
-import { AdminUserService } from "src/app/services/user/admin-user.service";
-import { HttpEvent, HttpEventType } from "@angular/common/http";
-import { CustomToastrService } from "src/app/services/helper/custom-toastr.service";
-import { SetAdminUser } from "src/app/store/user/admin-user.action";
-import { RolesState } from "src/app/store/role.action";
-import { Role } from "src/app/_models/permission.model";
+} from '../.././../_shared/abstract-form-mode';
+import { ValidationService } from '../../../_shared/reactive-input/_shared/services/validation.service';
+import { AdminUserService } from 'src/app/services/user/admin-user.service';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { CustomToastrService } from 'src/app/services/helper/custom-toastr.service';
+import { SetAdminUser } from 'src/app/store/user/admin-user.action';
+import { RolesState } from 'src/app/store/role.action';
+import { Role } from 'src/app/_models/permission.model';
 
 @Component({
-  selector: "app-modal-form-admin",
-  templateUrl: "./modal-form-admin.component.html",
+  selector: 'app-modal-form-admin',
+  templateUrl: './modal-form-admin.component.html',
   styles: [],
 })
 export class ModalFormAdminComponent extends UserAdminForm
@@ -52,7 +52,7 @@ export class ModalFormAdminComponent extends UserAdminForm
           // -- Set role default on create
           this.subscriptionService = this.rolesData$.subscribe((roles) => {
             roles.find((role) => {
-              if (role.devName === "admin") {
+              if (role.devName === 'admin') {
                 this.form.controls.role.setValue(role.id);
               }
             });
@@ -79,8 +79,8 @@ export class ModalFormAdminComponent extends UserAdminForm
               case HttpEventType.Response:
                 this.store.dispatch(new SetAdminUser(response.body));
                 this.toast.registerSuccess(
-                  "Registro",
-                  "Usuario registrado satisfactoriamente"
+                  'Registro',
+                  'Usuario registrado satisfactoriamente'
                 );
                 this.onResetForm();
                 break;
--- src/app/pages/user/roles-and-permits/admin-user/table-form-admin/table-form-admin.component.ts
+++ src/app/pages/user/roles-and-permits/admin-user/table-form-admin/table-form-admin.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { AbstractSmartTable } from "../../../_shared/smart-table/abstract-smart-table";
-import { AdminUserState } from "src/app/store/user/admin-user.action";
-import { Select } from "@ngxs/store";
-import { AdminUser } from "src/app/_models/user/admin-user.model";
-import { Observable } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { AbstractSmartTable } from '../../../_shared/smart-table/abstract-smart-table';
+import { AdminUserState } from 'src/app/store/user/admin-user.action';
+import { Select } from '@ngxs/store';
+import { AdminUser } from 'src/app/_models/user/admin-user.model';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: "app-table-form-admin",
-  templateUrl: "./table-form-admin.component.html",
-  styleUrls: ["./table-form-admin.component.scss"],
+  selector: 'app-table-form-admin',
+  templateUrl: './table-form-admin.component.html',
+  styleUrls: ['./table-form-admin.component.scss'],
 })
 export class TableFormAdminComponent extends AbstractSmartTable
   implements OnInit {
@@ -21,16 +21,16 @@ export class TableFormAdminComponent extends AbstractSmartTable
   ngOnInit() {
     this.settings.columns = {
       firstName: {
-        title: "Nombre",
-        type: "string",
+        title: 'Nombre',
+        type: 'string',
       },
       lastName: {
-        title: "Apellido",
-        type: "string",
+        title: 'Apellido',
+        type: 'string',
       },
       function: {
-        title: "Cargo",
-        type: "string",
+        title: 'Cargo',
+        type: 'string',
       },
     };
   }

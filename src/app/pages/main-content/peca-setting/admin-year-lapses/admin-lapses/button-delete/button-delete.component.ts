888768d951453af79c7f87f46b76d657751b85ec
franklinp <franklin@binauraldev.com>
2020-07-22 20:57:09 -0400

    WIP: 70% del backoffice tiene implementado el dialogo de confirmacion

--- src/app/pages/main-content/peca-setting/admin-year-lapses/admin-lapses/button-delete/button-delete.component.ts
+++ src/app/pages/main-content/peca-setting/admin-year-lapses/admin-lapses/button-delete/button-delete.component.ts
@@ -4,33 +4,65 @@ import { Store } from '@ngxs/store';
 import { DeleteLapseActivity } from 'src/app/store/lapse-activities.action';
 import { CustomToastrService } from 'src/app/services/helper/custom-toastr.service';
 import { MenuSetUp } from 'src/app/pages/pages-menu-service';
+import { DialogConfirmationComponent } from 'src/app/pages/_components/shared/dialog/dialog-confirmation/dialog-confirmation.component';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-button-delete',
   templateUrl: './button-delete.component.html',
-  styleUrls: ['./button-delete.component.scss']
+  styleUrls: ['./button-delete.component.scss'],
 })
 export class ButtonDeleteComponent implements OnInit {
-
   @Input() value: any;
   @Input() rowData: any;
 
   @Output() save: EventEmitter<any> = new EventEmitter();
 
+  subscription: Subscription;
+
   constructor(
+    private modalServicesBs: BsModalService,
     private menuService: MenuSetUp,
     private toastr: CustomToastrService,
     private store: Store,
-    private lapseActivityService: LapseActivitiesService ) { }
+    private lapseActivityService: LapseActivitiesService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onclick() {
-    this.lapseActivityService.deleteActivity(this.rowData.id, this.rowData.lapse).subscribe( response => {
-      this.toastr.deleteRegister('Eliminación', 'Se ha eliminado una actividad');
-      this.store.dispatch( new DeleteLapseActivity( this.rowData.id, this.rowData.lapse ) );
-      this.menuService.renderMenu(true);
-    } );
+    const modal = this.modalServicesBs.show(
+      DialogConfirmationComponent,
+      Object.assign({}, { class: 'modal-dialog-centered' })
+    );
+
+    // -- Set up modal
+    (modal.content as DialogConfirmationComponent).showConfirmationModal(
+      'Eliminar actividad de lapso',
+      '¿Desea eliminar la actividad de lapso seleccionada?',
+      'Verifica el lapso'
+    );
+
+    this.subscription = (modal.content as DialogConfirmationComponent).onClose.subscribe(
+      (result) => {
+        if (result === true) {
+          this.lapseActivityService
+            .deleteActivity(this.rowData.id, this.rowData.lapse)
+            .subscribe((response) => {
+              this.toastr.deleteRegister(
+                'Eliminación',
+                'Se ha eliminado una actividad'
+              );
+              (modal.content as DialogConfirmationComponent).hideConfirmationModal();
+
+              this.store.dispatch(
+                new DeleteLapseActivity(this.rowData.id, this.rowData.lapse)
+              );
+              this.menuService.renderMenu(true);
+            });
+        }
+      }
+    );
   }
 }

456339a14b50ea8f356c64f59304d7bb179631bb
franklinp <franklin@binauraldev.com>
2020-07-22 20:45:36 -0400

    WIP: Correccion de actualzacion de tema con respecto al nivel escolar, dialogo de confirmacion implementado. 163#

--- src/app/pages/main-content/peca-setting/environmental-project/levels-form/levels-form.component.ts
+++ src/app/pages/main-content/peca-setting/environmental-project/levels-form/levels-form.component.ts
@@ -1,28 +1,38 @@
-import { Component, OnInit, Input, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnDestroy,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { Subscription, Observable } from 'rxjs';
 import { Store, Select } from '@ngxs/store';
 import { EnvironmentalProjectService } from 'src/app/services/environmental-project.service';
-import { EnvironmentalProjectState
-  , EnvironmentalProjectModel
-  , DeleteSchoolLevel
-  , UpdateSchoolLevel } from 'src/app/store/environmental-project.action';
+import {
+  EnvironmentalProjectState,
+  EnvironmentalProjectModel,
+  DeleteSchoolLevel,
+  UpdateSchoolLevel,
+} from 'src/app/store/environmental-project.action';
 import { FormGroup, FormControl } from '@angular/forms';
+import { DialogConfirmationComponent } from 'src/app/pages/_components/shared/dialog/dialog-confirmation/dialog-confirmation.component';
+import { BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-levels-form',
   templateUrl: './levels-form.component.html',
-  styleUrls: ['./levels-form.component.scss']
+  styleUrls: ['./levels-form.component.scss'],
 })
 export class LevelsFormComponent implements OnInit, OnDestroy {
-
-  @Select(EnvironmentalProjectState.environmentalProjectStorable) storable$: Observable<EnvironmentalProjectModel>;
-  @Select(EnvironmentalProjectState.environmentalProject) environmentalProjectSelected: Observable<EnvironmentalProjectModel>;
+  @Select(EnvironmentalProjectState.environmentalProjectStorable)
+  storable$: Observable<EnvironmentalProjectModel>;
+  @Select(EnvironmentalProjectState.environmentalProject)
+  environmentalProjectSelected: Observable<EnvironmentalProjectModel>;
   subscription: Subscription;
 
   @Input() indexTopic: number; // <-- Index Topic
   @Input() index: number; // <-- Index level
   @Input() options: any[];
-
 
   form: FormGroup;
 
@@ -35,48 +45,48 @@ export class LevelsFormComponent implements OnInit, OnDestroy {
   showProgress = false;
 
   constructor(
+    private modalServicesBs: BsModalService,
     private cd: ChangeDetectorRef,
     private environmentalProjectService: EnvironmentalProjectService,
-    private store: Store) { }
+    private store: Store
+  ) {}
 
   ngOnInit() {
-
-
     this.form = new FormGroup({
       week: new FormControl(),
-      duration: new FormControl()
+      duration: new FormControl(),
     });
 
     // -- Set the value to the form --
-    this.subscription = this.environmentalProjectSelected.subscribe(response => {
+    this.subscription = this.environmentalProjectSelected.subscribe(
+      (response) => {
+        response.lapseSelected.topics.forEach((topic, index) => {
+          if (this.indexTopic === index) {
+            if (topic.levels.length >= 0) {
+              topic.levels.forEach((value, key) => {
+                if (key === this.index) {
+                  this.form.patchValue(value);
 
-      response.lapseSelected.topics.forEach((topic, index) => {
+                  this.options =
+                    value.target.length > 0
+                      ? JSON.parse(JSON.stringify(value.target))
+                      : this.options;
 
-        if (this.indexTopic === index ) {
-
-          if (topic.levels.length >= 0) {
-
-            topic.levels.forEach((value, key) => {
-
-              if (key === this.index) {
-
-                this.form.patchValue(value);
-
-                this.options = value.target.length > 0 ? JSON.parse(JSON.stringify(value.target)) : this.options;
-
-                this.techniques = Object.assign([], value.techniques);
-                this.activities = Object.assign([], value.activities);
-                this.resources = Object.assign([], value.resources);
-                this.evaluations = Object.assign([], value.evaluations);
-                this.supportMaterial = Object.assign([], value.supportMaterial);
-              }
-
-            });
+                  this.techniques = Object.assign([], value.techniques);
+                  this.activities = Object.assign([], value.activities);
+                  this.resources = Object.assign([], value.resources);
+                  this.evaluations = Object.assign([], value.evaluations);
+                  this.supportMaterial = Object.assign(
+                    [],
+                    value.supportMaterial
+                  );
+                }
+              });
+            }
           }
-
-        }
-      });
-    });
+        });
+      }
+    );
 
     this.cd.detectChanges();
   }
@@ -89,49 +99,68 @@ export class LevelsFormComponent implements OnInit, OnDestroy {
 
   // -- Action to delete level --
   deleteHimself(): void {
+    const modal = this.modalServicesBs.show(
+      DialogConfirmationComponent,
+      Object.assign({}, { class: 'modal-dialog-centered' })
+    );
 
-    this.subscription = this.store.dispatch(new DeleteSchoolLevel(
-      this.indexTopic,
-      this.index
-    )).subscribe(() => {
+    // -- Set up modal
+    (modal.content as DialogConfirmationComponent).showConfirmationModal(
+      'Eliminar nivel escolar',
+      '¿Desea eliminar nivel escolar seleccionado?',
+      'Verificar el tema asociado antes de eliminar'
+    );
 
-      this.subscription = this.storable$.subscribe(value => {
-
-        this.subscription = this.environmentalProjectService.updateEnvironmentalProject(value).subscribe(response => {
-          // -- Successfully mock delete topic --
-        });
-      });
-
-    });
+    this.subscription = (modal.content as DialogConfirmationComponent).onClose.subscribe(
+      (result) => {
+        if (result === true) {
+          this.subscription = this.store
+            .dispatch(new DeleteSchoolLevel(this.indexTopic, this.index))
+            .subscribe(() => {
+              this.subscription = this.storable$.subscribe((value) => {
+                this.subscription = this.environmentalProjectService
+                  .updateEnvironmentalProject(value)
+                  .subscribe((response) => {
+                    // -- Successfully mock delete topic --
+                    (modal.content as DialogConfirmationComponent).hideConfirmationModal();
+                  });
+              });
+            });
+        }
+      }
+    );
   }
 
   async onUpdateLevel() {
-
     this.showProgress = true;
 
     setTimeout(() => {
-      this.subscription = this.store.dispatch(new UpdateSchoolLevel({
-        target: this.options,
-        week: this.form.controls.week.value,
-        duration: this.form.controls.duration.value,
-        techniques: this.techniques,
-        activities: this.activities,
-        resources: this.resources,
-        evaluations: this.evaluations,
-        supportMaterial: this.supportMaterial
-      },
-        this.indexTopic,
-        this.index
-      )).subscribe(() => {
-
-        this.subscription = this.storable$.subscribe(value => {
-          this.subscription = this.environmentalProjectService.updateEnvironmentalProject(value).subscribe(response => {
-            // -- Successfully mock delete topic --
+      this.subscription = this.store
+        .dispatch(
+          new UpdateSchoolLevel(
+            {
+              target: this.options,
+              week: this.form.controls.week.value,
+              duration: this.form.controls.duration.value,
+              techniques: this.techniques,
+              activities: this.activities,
+              resources: this.resources,
+              evaluations: this.evaluations,
+              supportMaterial: this.supportMaterial,
+            },
+            this.indexTopic,
+            this.index
+          )
+        )
+        .subscribe(() => {
+          this.subscription = this.storable$.subscribe((value) => {
+            this.subscription = this.environmentalProjectService
+              .updateEnvironmentalProject(value)
+              .subscribe((response) => {
+                // -- Successfully mock delete topic --
+              });
           });
         });
-      });
     });
-
-
   }
 }

8d6cdb62fb94c4e401e9dcf6d444a173c6c6bd8a
franklinp <franklin@binauraldev.com>
2020-07-30 16:49:31 -0400

    WIP: Acciones implementadas. #283

--- src/app/pages/main-content/peca-setting/activity-strategies/activity-strategies.component.ts
+++ src/app/pages/main-content/peca-setting/activity-strategies/activity-strategies.component.ts
@@ -3,6 +3,14 @@ import { Select, Store } from '@ngxs/store';
 import {
   ActivityStrategyState,
   AddSliderEnviromentToActivityStrategy,
+  UpdateSliderEnviromentToActivityStrategy,
+  DeleteSliderEnviromentToActivityStrategy,
+  AddSliderReadingToActivityStrategy,
+  UpdateSliderReadingToActivityStrategy,
+  DeleteSliderReadingToActivityStrategy,
+  AddSliderMathToActivityStrategy,
+  UpdateSliderMathToActivityStrategy,
+  DeleteSliderMathToActivityStrategy
 } from 'src/app/store/activity-strategy.action';
 import { Observable } from 'rxjs';
 import { Slider } from 'src/app/_models/web/slider.model';
@@ -25,9 +33,63 @@ export class ActivityStrategiesComponent implements OnInit {
 
   ngOnInit() {}
 
+  /**
+   * =====================
+   * Enviroment
+   * =====================
+   */
+
   onRegisterSliderEnviroment(event: any) {
     this.store.dispatch(new AddSliderEnviromentToActivityStrategy(event));
   }
 
+  onEditSliderEnviroment(event: any) {
+    this.store.dispatch(
+      new UpdateSliderEnviromentToActivityStrategy(event[1], event[0])
+    );
+  }
 
+  onDeleteSliderEnviroment(event: any) {
+    this.store.dispatch(new DeleteSliderEnviromentToActivityStrategy(event));
+  }
+
+  /**
+   * =====================
+   * Reading
+   * =====================
+   */
+
+  onRegisterSliderReading(event: any) {
+    this.store.dispatch(new AddSliderReadingToActivityStrategy(event));
+  }
+
+  onEditSliderReading(event: any) {
+    this.store.dispatch(
+      new UpdateSliderReadingToActivityStrategy(event[1], event[0])
+    );
+  }
+
+  onDeleteSliderReading(event: any) {
+    this.store.dispatch(new DeleteSliderReadingToActivityStrategy(event));
+  }
+
+  /**
+   * =====================
+   * Math
+   * =====================
+   */
+
+  onRegisterSliderMath(event: any) {
+    this.store.dispatch(new AddSliderMathToActivityStrategy(event));
+  }
+
+  onEditSliderMath(event: any) {
+    this.store.dispatch(
+      new UpdateSliderMathToActivityStrategy(event[1], event[0])
+    );
+  }
+
+  onDeleteSliderMath(event: any) {
+    this.store.dispatch(new DeleteSliderMathToActivityStrategy(event));
+  }
 }

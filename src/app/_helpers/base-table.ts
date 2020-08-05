2e6fa439d0fe8711f856a5c4c58ecbb06a49419f
franklinp <franklin@binauraldev.com>
2020-08-05 15:26:08 -0400

    WIP: CRUD implementado al usuario administrador. #124

--- src/app/_helpers/base-table.ts
+++ src/app/_helpers/base-table.ts
@@ -1,54 +1,65 @@
-import { ACTION } from './text-content/text-crud';
-import { LocalDataSource } from 'ng2-smart-table';
+import { ACTION } from "./text-content/text-crud";
+import { LocalDataSource } from "ng2-smart-table";
 
 export class BaseTable {
+  ID_FORM: string; // <-- To relate it to some form
+  ACTION = ACTION; // <-- Attr: CREATE, EDIT, VIEW and EDIT
+  MODE; // <-- Create or update mode
 
-    ID_FORM: string; // <-- To relate it to some form
-    ACTION = ACTION; // <-- Attr: CREATE, EDIT, VIEW and EDIT
-    MODE; // <-- Create or update mode
+  // Settings
+  source: LocalDataSource = new LocalDataSource();
+  settings: any = {
+    noDataMessage: "No hay registros",
+    mode: "external",
+    actions: {
+      columnTitle: "Acciones",
+      add: false,
+      edit: false,
+      //  Fake action
+      delete: true,
+      custom: [
+        { name: ACTION.VIEW, title: '<i class="far fa-eye fa-sm"></i>' },
+        { name: ACTION.EDIT, title: `<i class="nb-edit"></i>` },
+        { name: ACTION.DELETE, title: '<i class="nb-trash"></i>' },
+      ],
+    },
+    // Fake column cuz a bug
+    delete: {
+      deleteButtonContent: '<i class="ion-trash-a"></i>',
+      confirmDelete: true,
+    },
+    pager: {
+      display: true,
+      perPage: 10,
+    },
+    columns: {},
+  };
 
-    // Settings
-    source: LocalDataSource = new LocalDataSource();
-    settings: any = {
-        noDataMessage: 'No hay registros',
-        mode: 'external',
-        actions: {
-            columnTitle: 'Acciones',
-            add: false,
-            edit: false,
-            //  Fake action
-            delete: true,
-            custom: [
-                { name: ACTION.VIEW, title: '<i class="far fa-eye fa-sm"></i>' },
-                { name: ACTION.EDIT, title: `<i class="nb-edit"></i>` },
-                { name: ACTION.DELETE, title: '<i class="nb-trash"></i>' }
-            ]
-        },
-        // Fake column cuz a bug
-        delete: {
-            deleteButtonContent: '<i class="ion-trash-a"></i>',
-            confirmDelete: true
-        },
-        pager: {
-            display: true,
-            perPage: 10
-        },
-        columns: {}
-    };
-
-    constructor(ID_FORM?: string) {
-        this.ID_FORM = ID_FORM;
+  public validateAction(edit: boolean, remove: boolean) {
+    if (edit) {
+      this.settings.actions.custom = this.settings.actions.custom.filter(
+        (obj) => obj.name !== ACTION.EDIT
+      );
     }
+
+    if (remove) {
+      this.settings.actions.custom = this.settings.actions.custom.filter(
+        (obj) => obj.name !== ACTION.DELETE
+      );
+    }
+  }
+
+  constructor(ID_FORM?: string) {
+    this.ID_FORM = ID_FORM;
+  }
 }
 
 // Normal actions CRUD users
 export interface TableActions {
-    deleteData?: (data: any) => void;
-    newData?: (data: any) => void;
-    updateData?: (data: any) => void;
-    onAction?: (event: any) => void;
+  deleteData?: (data: any) => void;
+  newData?: (data: any) => void;
+  updateData?: (data: any) => void;
+  onAction?: (event: any) => void;
 }
 
-export class SmartTable {
-
-}
+export class SmartTable {}

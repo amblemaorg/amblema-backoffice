f3071147110dd9ebb26402517c76326a4c790ce7
José Guerrero <jose.guerrero@binaural.com.ve>
2020-09-29 17:13:01 -0400

    Agrega URLs en enviroment.prod.ts con el dominio para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'http://167.71.187.213/api/',
-  web: 'http://167.71.187.213/'
+  api: 'https://amblema.org/api/',
+  web: 'https://amblema.org/'
 };

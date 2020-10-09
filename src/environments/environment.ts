521297582eedfbdf34a9d3b2fa6679a6d5e3088a
franklinp <franklin@binauraldev.com>
2020-10-09 11:36:57 -0400

    IMP: Configuraciones especiales para el ambiente de produccion.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: true,
-  //api: 'https://amblema.org/api/', // <--- QA
+  api: 'https://amblema.org/api/',
   // api: 'http://localhost:10506/',
   // api: 'http://186.88.121.11:10505/',
   // -- Web page Amblema --
-  //web: 'https://amblema.org/'
+  web: 'https://amblema.org/'
 
-  api: "http://157.245.131.248:10506/", 
-  web: "http://157.245.131.248:10512/", 
+  // api: "http://157.245.131.248:10506/", 
+  // web: "http://157.245.131.248:10512/", 
 };
 
 /*

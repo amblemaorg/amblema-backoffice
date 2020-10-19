716a89d7586db8450977ad6716cb45b2f676ec71
franklinp <franklin@binauraldev.com>
2020-10-19 09:55:09 -0400

    IMP: Se han agregado las mayussculas #286

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   //api: 'https://amblema.org/api/',
   // api: 'http://localhost:10506/',
   // api: 'http://186.88.121.11:10505/',

ff49ba3339fc14cc347f17cf2a58e0670f7bf1db
franklinp <franklin@binauraldev.com>
2020-12-08 12:18:13 -0400

    IMP: Preparando ambiente de produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,16 @@
 
 export const environment = {
   production: true,
-  //api: "https://amblema.org/api/",
-  //web: "https://amblema.org/",
+  api: "https://amblema.org/api/",
+  web: "https://amblema.org/",
 
   // api: 'http://localhost:10506/',
   // api: 'http://186.88.121.11:10505/',
   // -- Web page Amblema --
 
-  api: "http://157.245.131.248:10506/",
-  web: "http://157.245.131.248:10512/",
+  // api: "http://157.245.131.248:10506/",
+  // web: "http://157.245.131.248:10512/",
+
 };
 
 /*

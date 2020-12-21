3d0c411ad1d46e30f84be3118070839c372af19f
franklinp <franklin@binauraldev.com>
2020-12-21 10:19:40 -0400

    WIP: Preparando ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  // api: "https://amblema.org/api/",
-  // web: "https://amblema.org/",
-
-  // api: 'http://localhost:10506/',
-  // api: 'http://186.88.121.11:10505/',
-  // -- Web page Amblema --
+  production: false,
 
   api: 'http://157.245.131.248:10506/',
   web: 'http://157.245.131.248:10512/',

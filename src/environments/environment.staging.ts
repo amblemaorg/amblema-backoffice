3653535c1e5c4157636499227a39c7a5f80cb30a
franklinp <franklin@binauraldev.com>
2021-01-27 16:06:13 -0400

    COR: Resolucion de conflictos

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,7 +3,6 @@ export const environment = {
   // api: "https://amblema.org/api/", // <--
   // web: `https://amblema.org/`,
 
-<<<<<<< HEAD
   // api: 'http://localhost:10506/',
   // api: 'http://186.88.121.11:10505/',
   
@@ -11,10 +10,4 @@ export const environment = {
 
   api: 'http://157.245.131.248:10516/',
   web: 'http://157.245.131.248:10512/',
-=======
-  // api: 'http://157.245.131.248:10516/',
-  // web: 'http://157.245.131.248:10512/',
-  // -- Web page Amblema --
-
->>>>>>> 977ee9a718429ba425c856f92e4fbf88896cbdae
 };

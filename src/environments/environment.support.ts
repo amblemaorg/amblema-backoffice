9c7a07e5cebda31c0507a23e79ff2792052c4033
franklinp <franklin@binauraldev.com>
2021-01-29 17:32:21 -0400

    IMP: Variables de entorno configuradas

--- src/environments/environment.support.ts
+++ src/environments/environment.support.ts
@@ -4,16 +4,8 @@
 
 export const environment = {
     production: true,
-    // api: "https://amblema.org/api/",
-    // web: "https://amblema.org/",
-  
-    // api: 'http://157.245.131.248:10506/',
-    // web: 'http://157.245.131.248:10512/',
-  
     api: 'http://157.245.131.248:10516/',
     web: 'http://157.245.131.248:10512/',
-
-
 };
   
   /*

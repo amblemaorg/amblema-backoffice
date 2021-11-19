8a91b5d266b2fd013222bb8e13e48244d697e89a
Fidel <fidel.aliaga@binauraldev.com>
2021-11-19 17:48:28 -0400

    fix: correccion en comentario solicitud

--- src/app/services/request/information-request.service.ts
+++ src/app/services/request/information-request.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { map } from 'rxjs/operators';
-import { RequestContent } from 'src/app/_models/request/request-content-approval.model';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { map } from "rxjs/operators";
+import { RequestContent } from "src/app/_models/request/request-content-approval.model";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class InformationRequestService {
-  private readonly REQUEST_CONTENT_APPROVAL = 'requestscontentapproval';
+  private readonly REQUEST_CONTENT_APPROVAL = "requestscontentapproval";
 
   constructor(private httpClient: HttpClient) {}
 
@@ -24,20 +24,18 @@ export class InformationRequestService {
       .pipe(map((data: any) => data.records));
   }
 
-  updateRequestContentApproval(
-    data: RequestContent
-  ): Observable<any> {
+  updateRequestContentApproval(data: RequestContent): Observable<any> {
     return this.httpClient.put<any>(
       `${environment.api}${this.REQUEST_CONTENT_APPROVAL}/${data.id}`,
-      { status: data.status, comment: data.comments },
-      { reportProgress: true, observe: 'body' }
+      { status: data.status, comments: data.comments },
+      { reportProgress: true, observe: "body" }
     );
   }
 
   deleteRequestContentApproval(id: string): Observable<any> {
     return this.httpClient.delete<string>(
       `${environment.api}${this.REQUEST_CONTENT_APPROVAL}/${id}`,
-      { reportProgress: true, observe: 'body' }
+      { reportProgress: true, observe: "body" }
     );
   }
 }

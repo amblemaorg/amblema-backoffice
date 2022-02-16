43ad239e8c5931a6af7f49bd2e67d33a04b8cab6
Christopher <christopher@binauraldev.com>
2022-02-16 17:01:27 -0400

    WIP: #332 Implementar editor de texto para poder colocar saltos de línea en texto de los AmbLeTemas
    * Renamed text-editor-component to textarea-editor-component.
    
    * TextareaEditorComponent: trying to keep the characters limit.

--- src/app/pages/_components/shared/shared.module.ts
+++ src/app/pages/_components/shared/shared.module.ts
@@ -1,28 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { QuillModule } from "ngx-quill";
-import { TextEditorComponent } from "./text-editor/text-editor.component";
-import { ReactiveFormsModule } from "@angular/forms";
 
-const components = [TextEditorComponent];
+const components = [];
 
 @NgModule({
   declarations: components,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    QuillModule.forRoot({
-      modules: {
-        toolbar: [
-          ["bold", "italic", "underline", "strike"],
-          // [{ list: "ordered" }, { list: "bullet" }],
-          // [{ header: [2, 3, 4, 5, 6, false] }],
-          ["link"],
-          ["blockquote"],
-        ],
-      },
-    }),
-  ],
+  imports: [CommonModule],
   exports: components,
 })
 export class SharedModule {}

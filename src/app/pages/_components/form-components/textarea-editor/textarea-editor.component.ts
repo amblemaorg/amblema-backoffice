02d6fae8a62f164c2ed31282bdd4ee4550bf7c99
Christopher <christopher@binauraldev.com>
2022-02-17 12:03:30 -0400

    WIP: #332 Implementar editor de texto para poder colocar saltos de línea en texto de los AmbLeTemas
    * TextareaEditorComponent:
    
    - Created new custom Validator to set as valid or not, just taking account characters lenght not html tags.
    
    - trying to keep the characters limit

--- src/app/pages/_components/form-components/textarea-editor/textarea-editor.component.ts
+++ src/app/pages/_components/form-components/textarea-editor/textarea-editor.component.ts
@@ -3,8 +3,11 @@ import {
   ChangeDetectorRef,
   Component,
   Input,
+  OnChanges,
+  SimpleChanges,
 } from "@angular/core";
-import { StringHelper } from "src/app/_helpers/string.helper";
+import { AbstractControl, ValidatorFn, Validators } from "@angular/forms";
+import { RegExpEnum, StringHelper } from "src/app/_helpers/string.helper";
 import { AbstractReactive } from "../abstract-reactive";
 
 @Component({
@@ -22,43 +25,68 @@ export class TextareaEditorComponent
 
   length = 0;
   value = "";
+  previousValue = "";
   show = false;
 
   constructor(private cd: ChangeDetectorRef) {
     super();
   }
 
-  ngOnInit(): void {}
-
-  ngAfterContentChecked() {
+  ngOnInit(): void {
     if (this.control) {
       this.show = true;
+
+      // this.control.setValidators(Validators.maxLength(10));
+      this.control.setValidators(this.maxLengthValidator(10));
+
       if (this.maxLength > 0) {
-        // if (!this.control.value) {
-        //   this.value = "";
-        // }
+        this.control.valueChanges.subscribe((valueChanges) => {
+          if (this.length <= this.maxLength) {
+            this.value = this.control.value ? this.control.value : "";
+          }
 
-        // const planeText = StringHelper.replaceHtmlTags(this.value);
+          this.length = StringHelper.replaceHtmlTags(this.value).length;
 
-        // if (planeText.length < this.maxLength) {
-        //   this.value = this.control.value;
-        // }
+          // if (this.length <= this.maxLength) {
+          //   this.control.setValidators;
+          // }
 
-        // // this.value =
-        // //   planeText.length < this.maxLength ? this.control.value : this.value;
-        // // this.value = this.control.value ? this.control.value : "";
-
-        // this.length = this.value ? planeText.length : 0;
-
-        this.value = this.control.value ? this.control.value : "";
-        this.length = StringHelper.replaceHtmlTags(this.value).length;
-
-        // console.clear();
-        // console.log("this.control.value", this.control);
-        // console.log("this.length", this.length);
-        // console.log("this.value", this.value);
+          console.log("this.value", this.value);
+          console.log("this.control.valid", this.control.valid);
+        });
       }
     }
-    this.cd.detectChanges();
+  }
+
+  ngAfterContentChecked() {
+    // if (this.control) {
+    //   this.show = true;
+    //   if (this.maxLength > 0) {
+    //     this.value = this.control.value ? this.control.value : "";
+    //     this.length = StringHelper.replaceHtmlTags(this.value).length;
+    //     // if (this.length > this.maxLength) {
+    //     //   // this.value = this.control.value;
+    //     //   if(this.value.match(RegExpEnum.htmlTags)) {
+    //     //   }
+    //     // }
+    //     // console.clear();
+    //     // console.log("this.control", this.control);
+    //     // console.log("this.length", this.length);
+    //     // console.log("this.value", this.control.value);
+    //   }
+    // }
+    // this.cd.detectChanges();
+  }
+
+  private maxLengthValidator(maxLength: number): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      if (
+        // (control.value !== undefined || control.value !== null) &&
+        StringHelper.replaceHtmlTags(control.value).length > maxLength
+      ) {
+        return { planeTextMaxLength: true };
+      }
+      return null;
+    };
   }
 }

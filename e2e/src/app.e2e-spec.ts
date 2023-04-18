ccec47615358d0936236030a26d748079af4ab3b
Zaidibeth Ramos <zaidi.ramos@gmail.com>
2023-04-17 20:30:24 -0400

    Cambio de Amblema a AmbLeMa en logs

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Amblema-BackOffice app is running!');
+    expect(page.getTitleText()).toEqual('AmbLeMa-BackOffice app is running!');
   });
 
   afterEach(async () => {
